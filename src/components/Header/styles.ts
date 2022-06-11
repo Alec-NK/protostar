c06889a04a3595d1147a8b66a089c5837be1e154
Alec-NK <alec.arquivos@gmail.com>
2022-06-11 18:24:35 -0300

    fix: ajuste no header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  grid-area: header;
-  width: 100%;
-  border-bottom: solid 2px #404040;
+    display: flex;
+    flex-direction: row;
+    grid-area: header;
+    width: 100%;
+    border-bottom: solid 1px #404040;
 `;
