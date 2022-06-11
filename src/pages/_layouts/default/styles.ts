e8e9782b3eec7d63f56402d8a86a1027d46d6632
Alec-NK <alec.arquivos@gmail.com>
2022-06-11 18:25:41 -0300

    fix: ajustes no layout

--- src/pages/_layouts/default/styles.ts
+++ src/pages/_layouts/default/styles.ts
@@ -1,19 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: grid;
-  grid-template-columns: auto 85%;
-  grid-template-rows: 6% auto;
-  grid-template-areas:
-    "sidebar header"
-    "sidebar content";
-  width: 100vw;
-  height: 100vh;
+    display: grid;
+    grid-template-columns: auto 85%;
+    grid-template-rows: 6% auto;
+    grid-template-areas:
+        "sidebar header"
+        "sidebar content";
+    width: 100vw;
+    height: 100vh;
 
-  .content {
-    display: flex;
-    flex-direction: column;
-    grid-area: content;
-    padding: 20px;
-  }
+    .content {
+        display: flex;
+        flex-direction: column;
+        grid-area: content;
+        padding: 40px 30px;
+        overflow: auto;
+    }
 `;
