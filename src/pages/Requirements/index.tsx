82c905032fd5266ce4b57a86c2890b23c5a4b679
Alec-NK <alec.arquivos@gmail.com>
2022-06-12 19:25:19 -0300

    feat: estrutura tela requisitos

--- src/pages/Requirements/index.tsx
+++ src/pages/Requirements/index.tsx
@@ -1,5 +1,102 @@
+import { useState } from "react";
+import { BsPlusLg } from "react-icons/bs";
+
+import Button from "../../components/Button";
+import ItemTable from "./ItemTable";
+import RegisterModal from "./Modals/RegisterModal";
+
+import {
+    ButtonSection,
+    Container,
+    ContentTable,
+    Header,
+    HeaderTable,
+    SectionsPage,
+    TableList,
+} from "./styles";
+
 const Requirements = () => {
-  return <div>Requisito</div>;
+    const [isPageFunctional, setIsPageFunctional] = useState(true);
+    const [isPageNFunctional, setIsPageNFunctional] = useState(false);
+    const [isModalRegisterOpen, setIsModalRegisterOpen] = useState(false);
+
+    const toggleModalRegister = () => {
+        setIsModalRegisterOpen((prevState) => !prevState);
+    };
+
+    const toggleFunctionalPage = () => {
+        setIsPageFunctional(true);
+        setIsPageNFunctional(false);
+    };
+
+    const toggleNFunctionalPage = () => {
+        setIsPageFunctional(false);
+        setIsPageNFunctional(true);
+    };
+
+    return (
+        <Container>
+            <Header>
+                <h1>REQUISITOS</h1>
+                <Button
+                    text="NOVO"
+                    icon={<BsPlusLg />}
+                    onClick={toggleModalRegister}
+                />
+            </Header>
+            <SectionsPage>
+                <ButtonSection
+                    isActive={isPageFunctional}
+                    onClick={toggleFunctionalPage}
+                >
+                    Funcionais
+                </ButtonSection>
+                <ButtonSection
+                    isActive={isPageNFunctional}
+                    onClick={toggleNFunctionalPage}
+                >
+                    Não Funcionais
+                </ButtonSection>
+            </SectionsPage>
+            <TableList>
+                <HeaderTable>
+                    <div>ID</div>
+                    <div>Descrição</div>
+                    <div style={{ textAlign: "center" }}>Status</div>
+                    <div style={{ textAlign: "center" }}>Data de Criacao</div>
+                    <div style={{ textAlign: "center" }}>Versão</div>
+                    <div style={{ textAlign: "center" }}>Funções</div>
+                </HeaderTable>
+                <ContentTable>
+                    {isPageFunctional && (
+                        <>
+                            <ItemTable />
+                            <ItemTable />
+                            <ItemTable />
+                            <ItemTable />
+                            <ItemTable />
+                            <ItemTable />
+                            <ItemTable />
+                            <ItemTable />
+                            <ItemTable />
+                            <ItemTable />
+                        </>
+                    )}
+
+                    {isPageNFunctional && (
+                        <>
+                            <ItemTable />
+                            <ItemTable />
+                            <ItemTable />
+                        </>
+                    )}
+                </ContentTable>
+            </TableList>
+            {isModalRegisterOpen && (
+                <RegisterModal setIsOpen={toggleModalRegister} />
+            )}
+        </Container>
+    );
 };
 
 export default Requirements;
