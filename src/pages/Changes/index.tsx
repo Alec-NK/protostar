a48e03e899c004f6049ce696d13b4059654e37e8
Alec-NK <alec.arquivos@gmail.com>
2022-06-14 14:59:09 -0300

    feat: add estrutura pagina mudança

--- src/pages/Changes/index.tsx
+++ src/pages/Changes/index.tsx
@@ -1,5 +1,39 @@
+import {
+    ButtonSection,
+    Container,
+    ContentTable,
+    Header,
+    HeaderTable,
+    TableList,
+} from "./styles";
+
 const Changes = () => {
-  return <div>Mudanças</div>;
+    return (
+        <Container>
+            <Header>
+                <h1>MUDANÇAS</h1>
+                {/* <Button
+          text="SOLICITAR MUDANÇA"
+          icon={<BsPlusLg />}
+          onClick={}
+      /> */}
+            </Header>
+            <TableList>
+                <HeaderTable>
+                    <div>ID</div>
+                    <div>Data</div>
+                    <div>Titulo</div>
+                    <div>Status</div>
+                    <div>Solicitante</div>
+                    <div>Responsável</div>
+                    <div>Funções</div>
+                </HeaderTable>
+                <ContentTable>
+                    <div>asd</div>
+                </ContentTable>
+            </TableList>
+        </Container>
+    );
 };
 
 export default Changes;
