3f362dd77873e081308d85569e7c8f3cae97cbd6
Alec-NK <alec.arquivos@gmail.com>
2022-06-18 01:00:30 -0300

    feat: add types util

---
+++ src/util/Types.ts
@@ -0,0 +1,6 @@
+export const StatusTypes = [{ value: "TD", label: "To-do" }];
+
+export const RequirementsTypes = [
+    { value: "F", label: "Funcional" },
+    { value: "NF", label: "Não Funcional" },
+];
