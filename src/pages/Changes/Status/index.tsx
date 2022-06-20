e084ad7db66ea3140328a61bfd652c5c4923d5ab
Alec-NK <alec.arquivos@gmail.com>
2022-06-20 13:45:23 -0300

    fix: modificando e add status mudanca

--- src/pages/Changes/Status/index.tsx
+++ src/pages/Changes/Status/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { ChangesStatusEnum } from "../../../util/Enums";
 
 import { Container } from "./styles";
 
@@ -15,29 +16,75 @@ const Status = ({ typeStatus }: StatusProps) => {
 
     useEffect(() => {
         switch (typeStatus) {
-            case "Implementado":
+            case ChangesStatusEnum.analise:
+                setStatusData({
+                    label: "Em análise",
+                    textColor: "#e4bd23",
+                    backColor: "#FFF0B3",
+                });
+                break;
+            case ChangesStatusEnum.aprovado:
+                setStatusData({
+                    label: "Aprovado",
+                    textColor: "#08b489",
+                    backColor: "#c9f1e7",
+                });
+                break;
+            case ChangesStatusEnum.rejeitado:
+                setStatusData({
+                    label: "Rejeitado",
+                    textColor: "#DE350B",
+                    backColor: "#FFBDAD",
+                });
+                break;
+            case ChangesStatusEnum.bloqueado:
+                setStatusData({
+                    label: "Bloqueado",
+                    textColor: "#505F79",
+                    backColor: "#C1C7D0",
+                });
+                break;
+            case ChangesStatusEnum.emImplementação:
+                setStatusData({
+                    label: "Em implementação",
+                    textColor: "#2684FF",
+                    backColor: "#DEEBFF",
+                });
+                break;
+            case ChangesStatusEnum.implementado:
                 setStatusData({
                     label: "Implementado",
+                    textColor: "#084baf",
+                    backColor: "#B3D4FF",
+                });
+                break;
+            case ChangesStatusEnum.verificando:
+                setStatusData({
+                    label: "Verificando",
+                    textColor: "#5243AA",
+                    backColor: "#EAE6FF",
+                });
+                break;
+            case ChangesStatusEnum.finalizado:
+                setStatusData({
+                    label: "Finalizado",
                     textColor: "#198511",
-                    backColor: "#7dba79",
+                    backColor: "#dff7e2",
                 });
                 break;
 
             default:
                 setStatusData({
-                    label: "Teste",
-                    textColor: "#333333",
-                    backColor: "#afafaf",
+                    label: "None",
+                    textColor: "#505F79",
+                    backColor: "#C1C7D0",
                 });
                 break;
         }
     }, [typeStatus]);
 
     return (
-        <Container
-            color={statusData.textColor}
-            backgroundColor={statusData.backColor}
-        >
+        <Container color={statusData.textColor} backgroundColor={statusData.backColor}>
             <span>{statusData.label}</span>
         </Container>
     );
