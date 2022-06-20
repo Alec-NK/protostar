b8b4269eeb70f2f1331f9bce8a3b6144c37dc715
Alec-NK <alec.arquivos@gmail.com>
2022-06-20 13:44:25 -0300

    feat: add enum

---
+++ src/util/Enums.ts
@@ -0,0 +1,10 @@
+export enum ChangesStatusEnum {
+    analise = "AN",
+    aprovado = "APR",
+    rejeitado = "RJ",
+    bloqueado = "PR",
+    emImplementação = "ING",
+    implementado = "IED",
+    verificando = "VE",
+    finalizado = "D",
+}
