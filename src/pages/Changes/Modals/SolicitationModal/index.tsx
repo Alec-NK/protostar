0157a1d97707617563b3bec0cca4ebcfdbc9931f
Alec-NK <alec.arquivos@gmail.com>
2022-06-20 13:50:26 -0300

    feat: add modal solicitar mudanca

---
+++ src/pages/Changes/Modals/SolicitationModal/index.tsx
@@ -0,0 +1,234 @@
+import { useEffect, useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
+
+import AsyncSelect from "react-select/async";
+import Select from "react-select";
+
+import { AsyncSelectInputValuesType } from "../../../Requirements/Modals/RegisterModal";
+import { ChangesTypes, RequirementsTypes, StatusTypes } from "../../../../util/Types";
+
+import { Background, Container, Content, Footer, ModalHeader } from "./styles";
+
+type RegisterModalProps = {
+    setIsOpen: () => void;
+    reloadPage: () => void;
+};
+
+type Inputs = {
+    title: string;
+    reason: string;
+    status: any;
+    requestor: string;
+    accountable: string;
+    requirements: AsyncSelectInputValuesType;
+};
+
+const schema = yup
+    .object({
+        title: yup.string().required("Título obrigatório!"),
+        reason: yup.string().required("Motivo obrigatório!"),
+        status: yup
+            .object()
+            .shape({
+                value: yup.string(),
+                label: yup.string(),
+            })
+            .required("Status obrigatório!"),
+        accountable: yup.string().required("Responsável obrigatório!"),
+        requestor: yup.string().required("Solicitante obrigatório!"),
+    })
+    .required();
+
+const customStyles = {
+    control: (provided: any, state: any) => ({
+        ...provided,
+        padding: "0.3rem",
+        border: "2px solid #b3b3b3",
+        borderRadius: "5px",
+        fontSize: "15px",
+        outline: "none",
+        transition: "all 0.3s",
+        color: "black",
+        boxShadow: "none",
+        fontFamily: "Montserrat",
+        "&:hover": {
+            border: "2px solid #b3b3b3",
+        },
+    }),
+    menu: (provided: any, state: any) => ({
+        ...provided,
+        fontFamily: '"Montserrat", sans-serif;',
+    }),
+};
+
+const SolicitationModal = ({ setIsOpen, reloadPage }: RegisterModalProps) => {
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm<Inputs>({ resolver: yupResolver(schema) });
+    const [selectedChangeOption, setSelectedChangeOption] = useState<any>(ChangesTypes[0]);
+
+    const getRequirements = async () => {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/requisitos/`);
+        const options = await response.data.map((req: any) => ({
+            value: req.id,
+            label:
+                req.type === "F"
+                    ? `Requisito ${req.id} Funcional`
+                    : `Requisito ${req.id} Não Funcional`,
+        }));
+
+        return options;
+    };
+
+    const toggleIsModalOpen = () => {
+        setIsOpen();
+    };
+
+    const onSubmit: SubmitHandler<any> = async (data: Inputs) => {
+        console.log("Send", data);
+
+        const newData = {
+            title: data.title,
+            reason: data.reason,
+            status:
+                data.status.value === undefined ? selectedChangeOption.value : data.status.value,
+            requestor: data.requestor,
+            accountable: data.accountable,
+            is_accepted: false,
+            new_req: null,
+            data_mudanca: null,
+            progrecao: [data.requirements.value],
+        };
+
+        console.log("Send2", newData);
+
+        await axios
+            .post(`${process.env.REACT_APP_API_URL}/pedido_mudanca/`, newData)
+            .then(() => {
+                toast.success("Solicitação enviada com sucesso!");
+                toggleIsModalOpen();
+                reloadPage();
+            })
+            .catch((erro) => {
+                console.log("Error", erro);
+                toast.error("Erro na solicitação");
+            });
+    };
+
+    const handleChangeStatus = (option: any) => {
+        setSelectedChangeOption(option);
+    };
+
+    return (
+        <Background>
+            <Container>
+                <ModalHeader>
+                    <h2 className="title">Solicitar Mudança</h2>
+                </ModalHeader>
+                <Content>
+                    <form id="form_new_requirement" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="caption">INFORMAÇÕES</div>
+                        <div className="row" id="firstRow">
+                            <div className="inputContainer">
+                                <label>Título</label>
+                                <input
+                                    type="text"
+                                    className="title"
+                                    {...register("title")}
+                                    placeholder="Insira um título para mudança"
+                                />
+                                <p className="error_message">{errors.title?.message}</p>
+                            </div>
+                            <div className="inputContainer">
+                                <label>Requisitos</label>
+                                <Controller
+                                    name="requirements"
+                                    control={control}
+                                    rules={{ required: true }}
+                                    render={({ field }) => (
+                                        <AsyncSelect
+                                            {...field}
+                                            isClearable
+                                            defaultOptions
+                                            placeholder="Selecione os requisitos relacionados"
+                                            loadOptions={getRequirements}
+                                            styles={customStyles}
+                                        />
+                                    )}
+                                />
+                            </div>
+                        </div>
+                        <div className="row" id="secondRow">
+                            <div className="inputContainer">
+                                <label>Motivo</label>
+                                <textarea
+                                    className="input_reason"
+                                    {...register("reason")}
+                                    placeholder="Insira o motivo da mudança"
+                                />
+                                <p className="error_message">{errors.reason?.message}</p>
+                            </div>
+                        </div>
+                        <div className="row" id="thirdRow">
+                            <div className="inputContainer">
+                                <label>Status</label>
+                                <Controller
+                                    name="status"
+                                    control={control}
+                                    render={({ field }) => {
+                                        return (
+                                            <Select
+                                                {...field}
+                                                value={selectedChangeOption}
+                                                options={ChangesTypes}
+                                                onChange={(option) => handleChangeStatus(option)}
+                                                styles={customStyles}
+                                                defaultValue={{ value: "AN", label: "Em análise" }}
+                                            />
+                                        );
+                                    }}
+                                />
+                                <p className="error_message">{errors.status?.message}</p>
+                            </div>
+                            <div className="inputContainer">
+                                <label>Responsável</label>
+                                <input
+                                    type="text"
+                                    {...register("accountable")}
+                                    placeholder="Responável pela mudança"
+                                />
+                                <p className="error_message">{errors.accountable?.message}</p>
+                            </div>
+                            <div className="inputContainer">
+                                <label>Solicitante</label>
+                                <input
+                                    type="text"
+                                    {...register("requestor")}
+                                    placeholder="Solicitante da mudança"
+                                />
+                                <p className="error_message">{errors.reason?.message}</p>
+                            </div>
+                        </div>
+                    </form>
+                </Content>
+                <Footer>
+                    <button type="submit" form="form_new_requirement" className="btnRegister">
+                        CADASTRAR
+                    </button>
+                    <button className="btnCancel" onClick={toggleIsModalOpen}>
+                        CANCELAR
+                    </button>
+                </Footer>
+            </Container>
+        </Background>
+    );
+};
+
+export default SolicitationModal;
---
+++ src/pages/Changes/Modals/SolicitationModal/styles.ts
@@ -0,0 +1,253 @@
+import AsyncSelect from "react-select/async";
+import styled from "styled-components";
+
+export const Background = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    z-index: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    width: 50%;
+    height: 60%;
+    border-radius: 6px;
+    background-color: white;
+`;
+
+export const ModalHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px;
+    padding-bottom: 10px;
+    border-bottom: solid 3px rgba(242, 242, 242, 1);
+    color: #1a1a1a;
+
+    h2 {
+        font-family: "Montserrat", sans-serif;
+        font-weight: 700;
+    }
+`;
+
+export const Content = styled.div`
+    padding: 20px;
+    width: 100%;
+    height: calc(100% - 65px);
+    overflow: auto;
+
+    label {
+        margin: 0 0 5px 5px;
+        font-family: "Montserrat", sans-serif;
+        font-weight: 500;
+        font-size: 15px;
+    }
+
+    input {
+        padding: 0.8rem;
+        border: 2px solid #b3b3b3;
+        border-radius: 5px;
+        font-size: 15px;
+        outline: none;
+        transition: all 0.3s;
+        color: black;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        font-family: "Montserrat", sans-serif;
+
+        &:disabled {
+            border: 2px solid rgba(242, 242, 242, 1);
+            cursor: not-allowed;
+
+            ::placeholder {
+                color: rgba(242, 242, 242, 1);
+            }
+        }
+    }
+
+    select {
+        padding: 0.8rem;
+        border: 2px solid #b3b3b3;
+        border-radius: 5px;
+        font-size: 15px;
+        outline: none;
+        transition: all 0.3s;
+        color: black;
+    }
+
+    .row {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 100%;
+        margin-bottom: 20px;
+
+        .inputContainer {
+            display: flex;
+            flex-flow: column nowrap;
+
+            .category {
+                width: 100%;
+            }
+
+            .error_message {
+                color: red;
+                font-family: "Maven Pro", sans-serif;
+                margin: 4px 0 0 5px;
+            }
+
+            .input_reason {
+                padding: 15px;
+                border: 2px solid #b3b3b3;
+                border-radius: 5px;
+                outline: none;
+                font-family: "Montserrat", sans-serif;
+                resize: vertical;
+                color: black;
+                transition: all 0.3s;
+                -webkit-appearance: none;
+                -moz-appearance: none;
+            }
+        }
+
+        #version {
+            input {
+                &:disabled {
+                    border: 2px solid transparent;
+                    cursor: not-allowed;
+
+                    ::placeholder {
+                        color: grey;
+                    }
+                }
+            }
+        }
+    }
+
+    .caption {
+        margin-bottom: 15px;
+        font-family: "Montserrat", sans-serif;
+        color: #b3b3b3;
+    }
+
+    #firstRow {
+        display: grid;
+        /* grid-template-columns: 25% 74.5%; */
+        grid-template-columns: 49.5% 49.5%;
+    }
+
+    #secondRow {
+        display: grid;
+        grid-template-columns: 100%;
+    }
+
+    #thirdRow {
+        display: grid;
+        grid-template-columns: 33% 33% 33%;
+    }
+
+    /* .input-container {
+        width: 280px;
+        position: relative;
+    }
+
+    .label {
+        position: absolute;
+        left: 15px;
+        top: 15px;
+        transition: all 0.2s;
+        padding: 0 2px;
+        z-index: 1;
+        color: #b3b3b3;
+    }
+
+    .text-input {
+        padding: 0.8rem;
+        width: 100%;
+        height: 100%;
+        border: 2px solid #2f2c45;
+        border-radius: 5px;
+        font-size: 15px;
+        outline: none;
+        transition: all 0.3s;
+        color: black;
+    }
+
+    .label::before {
+        content: "";
+        height: 5px;
+        background: white;
+        position: absolute;
+        left: 0px;
+        top: 10px;
+        width: 100%;
+        z-index: -1;
+    }
+
+    .text-input:focus {
+        border: 2px solid #7e4ccb;
+    }
+
+    .text-input:focus + .label,
+    .filled {
+        top: -10px;
+        color: #7e4ccb;
+        font-size: 14px;
+        font-weight: bold;
+    }
+
+    .text-input::placeholder {
+        font-size: 16px;
+        opacity: 0;
+        transition: all 0.3s;
+    }
+
+    .text-input:focus::placeholder {
+        opacity: 1;
+    } */
+`;
+
+export const Footer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    padding: 20px;
+    border-top: solid 3px rgba(242, 242, 242, 1);
+
+    button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 0;
+        padding: 20px 25px;
+        border: none;
+        border-radius: 5px;
+        font-family: "Maven Pro", sans-serif;
+    }
+
+    .btnRegister {
+        margin-right: 10px;
+        background-color: #fab039;
+        color: white;
+        transition: 0.2s;
+
+        &:hover {
+            background-color: #f0a732;
+            transition: 0.2s;
+        }
+    }
+
+    .btnCancel {
+        background-color: transparent;
+        border: solid 1px #fab039;
+        color: #fab039;
+    }
+`;
