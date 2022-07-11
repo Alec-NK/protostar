0f03dda6de8b0001bcf43b642adc3288dc5be588
Alec-NK <alec.arquivos@gmail.com>
2022-07-11 12:03:45 -0300

    feat: add statusInfo

---
+++ src/util/StatusInfos.ts
@@ -0,0 +1,50 @@
+export const ChangeStatusInfos = [
+    {
+        id: "AN",
+        label: "EM ANÁLISE",
+        textColor: "#e4bd23",
+        backColor: "#FFF0B3",
+    },
+    {
+        id: "APR",
+        label: "APROVADO",
+        textColor: "#08b489",
+        backColor: "#c9f1e7",
+    },
+    {
+        id: "RJ",
+        label: "REJEITADO",
+        textColor: "#DE350B",
+        backColor: "#FFBDAD",
+    },
+    {
+        id: "PR",
+        label: "BLOQUEADO",
+        textColor: "#505F79",
+        backColor: "#C1C7D0",
+    },
+    {
+        id: "ING",
+        label: "EM IMPLEMENTAÇÃO",
+        textColor: "#2684FF",
+        backColor: "#DEEBFF",
+    },
+    {
+        id: "IED",
+        label: "IMPLEMENTADO",
+        textColor: "#084baf",
+        backColor: "#B3D4FF",
+    },
+    {
+        id: "VE",
+        label: "VERIFICANDO",
+        textColor: "#5243AA",
+        backColor: "#EAE6FF",
+    },
+    {
+        id: "D",
+        label: "FINALIZADO",
+        textColor: "#198511",
+        backColor: "#dff7e2",
+    },
+];
