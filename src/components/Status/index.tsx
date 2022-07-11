b90b44cab9a9bd731f9bdbb61467a0f379e9abb7
Alec-NK <alec.arquivos@gmail.com>
2022-07-11 12:03:00 -0300

    feat: criando componente status

---
+++ src/components/Status/index.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+import { StatusKinds } from "../../util/Enums";
+import { ChangeStatusInfos } from "../../util/StatusInfos";
+
+import { Container } from "./styles";
+
+type StatusProps = {
+    status: string;
+    type: string;
+};
+
+const Status = ({ status, type }: StatusProps) => {
+    const [dataStatus, setDataStatus] = useState({
+        id: "NN",
+        label: "none",
+        textColor: "#505F79",
+        backColor: "#C1C7D0",
+    });
+
+    useEffect(() => {
+        if (type === StatusKinds.requirements) {
+            console.log("Testando");
+        } else if (type === StatusKinds.changes) {
+            ChangeStatusInfos.forEach((changeStatus) => {
+                if (changeStatus.id === status) {
+                    setDataStatus(changeStatus);
+                }
+            });
+        }
+    }, [status, type]);
+
+    return (
+        <Container color={dataStatus.textColor} backgroundColor={dataStatus.backColor}>
+            <span>{dataStatus.label}</span>
+        </Container>
+    );
+};
+
+export default Status;
---
+++ src/components/Status/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+type ContainerProps = {
+    color: string;
+    backgroundColor: string;
+};
+
+export const Container = styled.div<ContainerProps>`
+    span {
+        padding: 6px 10px;
+        border-radius: 4px;
+        background-color: ${(props) => props.backgroundColor};
+        color: ${(props) => props.color};
+        font-family: "Maven Pro", sans-serif;
+        font-weight: 600;
+        font-size: 12px;
+    }
+`;
