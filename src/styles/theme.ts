3bc6c51931490781b4e5df371905ce47fcff6cc9
Alec-NK <alec.arquivos@gmail.com>
2022-07-16 18:53:34 -0300

    feat: add novas cores ao tema

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,8 @@
 export const defaultTheme = {
     orange: "#fab039",
+    dark_orange: "#e69d29",
+    black: "#1A1A1A",
+    black_grey: "#1f1f1f",
+    white_grey: "#e8e8e8",
+    grey: "#56595e",
 };
