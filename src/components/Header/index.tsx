d5318a936d110149464f1db2bbaeed4e7c6348de
Alec-NK <alec.arquivos@gmail.com>
2022-08-26 19:13:02 -0300

    feat: add itens header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,23 @@
-import { Link, NavLink } from "react-router-dom";
+import { Avatar, Wrap, WrapItem } from "@chakra-ui/react";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 import { Container } from "./styles";
 
 const Header = () => {
-  return <Container></Container>;
+    return (
+        <Container>
+            <button>
+                <IoMdNotificationsOutline />
+            </button>
+            <button>
+                <Wrap>
+                    <WrapItem>
+                        <Avatar size="sm" name="Alec" src="" />
+                    </WrapItem>
+                </Wrap>
+            </button>
+        </Container>
+    );
 };
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    grid-area: header;
     display: flex;
     flex-direction: row;
-    grid-area: header;
+    justify-content: end;
+    align-items: center;
     width: 100%;
-    border-bottom: solid 1px #404040;
+    padding-right: 50px;
+    font-size: 25px;
+    border-bottom: solid 1px #c7c7c7;
+
+    button {
+        margin-left: 20px;
+    }
 `;
