7f1404bafe1150e0e253cf84498a2ead56eb10fc
Alec-NK <alec.arquivos@gmail.com>
2022-08-29 15:42:26 -0300

    feat: add route inicio

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 
 import Projects from "../pages/Projects";
+import Home from "../pages/Home";
 import Requirements from "../pages/Requirements";
 import Changes from "../pages/Changes";
 import Solicitations from "../pages/Solicitations";
@@ -14,6 +15,7 @@ const IndexRoutes = () => (
         <Route path="/" element={<SignIn />} />
         <Route path="/cadastro" element={<SignUp />} />
         <Route path="/projetos" element={<PrivateRoute element={Projects} />} />
+        <Route path="/inicio" element={<PrivateRoute element={Home} />} />
         <Route path="/requisitos" element={<PrivateRoute element={Requirements} />} />
         <Route path="/mudancas" element={<PrivateRoute element={Changes} />} />
         <Route path="/solicitacoes" element={<PrivateRoute element={Solicitations} />} />
