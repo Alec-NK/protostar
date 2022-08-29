9e284d478fc297548bc72a20e144e9b025ecad5d
Alec-NK <alec.arquivos@gmail.com>
2022-08-29 15:28:25 -0300

    feat: add pagina home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import { Container, Header } from "./styles";
+
+const Home = () => {
+    return (
+        <Container>
+            <Header>
+                <h1>Nome do Projeto</h1>
+            </Header>
+            <div>Descrição</div>
+            <div>Membros do projeto</div>
+        </Container>
+    );
+};
+
+export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Header = styled.div`
+    h1 {
+        font-family: "Prompt", sans-serif;
+        font-size: 45px;
+        color: #1a1a1a;
+    }
+`;
