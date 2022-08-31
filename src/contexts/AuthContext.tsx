0a9a201a18caa66593668a30135a3c6ce6b4e7a4
Alec-NK <alec.arquivos@gmail.com>
2022-08-31 11:38:09 -0300

    feat: login, autenticacao e logout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,8 +1,27 @@
-import React, { createContext } from "react";
+import React, { createContext, useCallback, useState } from "react";
+import { toast } from "react-toastify";
+import api from "../services/api";
+
+import { SignInInputs } from "../pages/SignIn";
+
+type User = {
+    id: number;
+    email: string;
+    password: string;
+    username: string;
+    selectedProject?: number;
+};
+
+type AuthData = {
+    user: User;
+    token: string;
+};
 
 type AuthContextProps = {
-    signIn: () => void;
+    user: User;
+    signIn: (dataSignIn: SignInInputs) => Promise<boolean>;
     authenticated: () => boolean;
+    signOut: () => void;
 };
 
 type AuthProviderProps = {
@@ -12,15 +31,88 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextProps);
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-    const signIn = () => {
-        console.log("Testando context");
+    const [data, setData] = useState<AuthData>(() => {
+        const token = localStorage.getItem("user_token");
+        const user = localStorage.getItem("user");
+
+        if (token && user) {
+            return { token, user: JSON.parse(user) };
+        }
+
+        return {} as AuthData;
+    });
+
+    const signIn = async (dataSignIn: SignInInputs) => {
+        let allowedLogin = false;
+        let token = "";
+        let userData: User = {
+            id: 0,
+            email: "",
+            password: "",
+            username: "",
+        };
+
+        await api
+            .post(`/auth/`, dataSignIn)
+            .then((response) => {
+                token = response.data.token;
+                localStorage.setItem("user_token", token);
+                allowedLogin = true;
+            })
+            .catch(() => {
+                toast.error("Nome ou senha inválidos");
+                allowedLogin = false;
+            });
+
+        await api
+            .get(`/usuarios/1`)
+            .then((response) => {
+                userData = response.data;
+                localStorage.setItem("user", JSON.stringify(userData));
+            })
+            .catch(() => {
+                console.log("Error user");
+            });
+
+        setData({
+            token,
+            user: userData,
+        });
+
+        return allowedLogin;
     };
 
     const authenticated = () => {
-        return true;
+        const token = localStorage.getItem("user_token");
+
+        if (token) {
+            return true;
+        }
+
+        return false;
+    };
+
+    const updateSelectedProject = useCallback((projectId: number) => {
+        const clonedUser: User = data.user;
+
+        clonedUser.selectedProject = projectId;
+
+        localStorage.setItem("user", JSON.stringify(clonedUser));
+        setData({
+            token: data.token,
+            user: clonedUser,
+        });
+    }, []);
+
+    const signOut = () => {
+        localStorage.removeItem("user_token");
+        localStorage.removeItem("user");
+        setData({} as AuthData);
     };
 
     return (
-        <AuthContext.Provider value={{ signIn, authenticated }}>{children}</AuthContext.Provider>
+        <AuthContext.Provider value={{ user: data.user, signIn, authenticated, signOut }}>
+            {children}
+        </AuthContext.Provider>
     );
 };
