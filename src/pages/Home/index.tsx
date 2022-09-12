b80c1b5ec8041fad77ed2a94c6555883a1bfdbd8
Alec-NK <alec.arquivos@gmail.com>
2022-09-11 21:23:49 -0300

    feat: add informações tela início

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,44 @@
-import { Container, Header } from "./styles";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import api from "../../services/api";
+
+import { ProjectsData } from "../Projects";
+
+import { Container, Header, Description, Members, SubHeading } from "./styles";
 
 const Home = () => {
+    const { user } = useContext(AuthContext);
+    const [project, setProject] = useState<ProjectsData>({} as ProjectsData);
+
+    const getProject = useCallback(async () => {
+        await api
+            .get(`/projeto/${user.selectedProject}`)
+            .then((response) => {
+                setProject(response.data);
+            })
+            .catch((error) => {
+                console.log("Erro projeto");
+            });
+    }, [user]);
+
+    useEffect(() => {
+        getProject();
+    }, [user, getProject]);
+
     return (
         <Container>
             <Header>
-                <h1>Nome do Projeto</h1>
+                <h1>{project.title}</h1>
             </Header>
-            <div>Descrição</div>
-            <div>Membros do projeto</div>
+            <div className="row">
+                <Description>
+                    <SubHeading>Descrição do projeto</SubHeading>
+                    <div>{project.description}</div>
+                </Description>
+                <Members>
+                    <SubHeading>Membros</SubHeading>
+                </Members>
+            </div>
         </Container>
     );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,11 +1,49 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    .row {
+        display: flex;
+        flex-direction: row;
+    }
+`;
 
 export const Header = styled.div`
+    margin-bottom: 20px;
+    padding: 25px;
+    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
+    background-color: white;
+
     h1 {
-        font-family: "Prompt", sans-serif;
-        font-size: 45px;
+        font-family: "Montserrat", sans-serif;
+        font-size: 35px;
+        font-weight: 700;
         color: #1a1a1a;
     }
+`;
+
+export const Description = styled.div`
+    width: 70%;
+    height: 30vh;
+    margin-right: 2%;
+    padding: 25px;
+    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
+    background-color: white;
+`;
+
+export const Members = styled.div`
+    width: 28%;
+    padding: 25px;
+    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
+    background-color: white;
+`;
+
+export const SubHeading = styled.div`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 600;
+    font-size: 20px;
+    margin-bottom: 25px;
+    color: #1a1a1a;
 `;
