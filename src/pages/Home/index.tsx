ea31812159e33dde8ee1888362bfcc380242e9f2
Alec-NK <alec.arquivos@gmail.com>
2022-09-28 14:27:18 -0300

    fix: ajustes na home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,40 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { Avatar, Badge } from "@chakra-ui/react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import api from "../../services/api";
 
 import { ProjectsData } from "../Projects";
 
-import { Container, Header, Description, Members, SubHeading } from "./styles";
+import { Container, Header, Description, Members, SubHeading, MembersList } from "./styles";
+
+type MemberType = {
+    id: number;
+    email: string;
+    password: string;
+    username: string;
+};
 
 const Home = () => {
     const { user } = useContext(AuthContext);
     const [project, setProject] = useState<ProjectsData>({} as ProjectsData);
+    const [members, setMembers] = useState<MemberType[]>([]);
 
-    const getProject = useCallback(async () => {
+    const getProjectMembers = useCallback(async () => {
+        let proj = {} as ProjectsData;
         await api
             .get(`/projeto/${user.selectedProject}`)
             .then((response) => {
+                proj = response.data;
                 setProject(response.data);
+            })
+            .catch((error) => {
+                console.log("Erro projeto");
+            });
+
+        await api
+            .get(`/usuarios/${proj.participants[0]}`)
+            .then((response) => {
+                setMembers([response.data]);
             })
             .catch((error) => {
                 console.log("Erro projeto");
@@ -22,8 +42,8 @@ const Home = () => {
     }, [user]);
 
     useEffect(() => {
-        getProject();
-    }, [user, getProject]);
+        getProjectMembers();
+    }, [user, getProjectMembers]);
 
     return (
         <Container>
@@ -36,7 +56,21 @@ const Home = () => {
                     <div>{project.description}</div>
                 </Description>
                 <Members>
-                    <SubHeading>Membros</SubHeading>
+                    <div className="header_members">
+                        <h2>Membros</h2>
+                        <Badge>1</Badge>
+                    </div>
+                    <MembersList>
+                        {members.length > 0 &&
+                            members.map((member, index) => {
+                                return (
+                                    <div key={index} className="card_member">
+                                        <Avatar name={member.username} size="sm" />
+                                        <div className="member_name">{member.username}</div>
+                                    </div>
+                                );
+                            })}
+                    </MembersList>
                 </Members>
             </div>
         </Container>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -13,7 +13,7 @@ export const Container = styled.div`
 export const Header = styled.div`
     margin-bottom: 20px;
     padding: 25px;
-    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
+    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.2);
     background-color: white;
 
     h1 {
@@ -29,15 +29,30 @@ export const Description = styled.div`
     height: 30vh;
     margin-right: 2%;
     padding: 25px;
-    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
+    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.2);
     background-color: white;
 `;
 
 export const Members = styled.div`
     width: 28%;
     padding: 25px;
-    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
+    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.2);
     background-color: white;
+
+    .header_members {
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        margin-bottom: 25px;
+
+        h2 {
+            font-family: "Montserrat", sans-serif;
+            font-weight: 600;
+            font-size: 20px;
+            color: #1a1a1a;
+            margin-right: 15px;
+        }
+    }
 `;
 
 export const SubHeading = styled.div`
@@ -46,4 +61,20 @@ export const SubHeading = styled.div`
     font-size: 20px;
     margin-bottom: 25px;
     color: #1a1a1a;
+`;
+
+export const MembersList = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    .card_member {
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        margin-bottom: 20px;
+
+        span {
+            margin-right: 10px;
+        }
+    }
 `;
