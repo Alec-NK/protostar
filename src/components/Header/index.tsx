8b3ac51a1f529bbce70c1554cb6ec180f2830680
Alec-NK <alec.arquivos@gmail.com>
2022-10-25 16:37:49 -0300

    feat: notificações no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,163 @@
-import { useCallback, useContext } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Avatar, Menu, MenuButton, MenuItem, MenuList, Wrap, WrapItem } from "@chakra-ui/react";
+import {
+    Avatar,
+    IconButton,
+    Link,
+    Menu,
+    MenuButton,
+    MenuGroup,
+    MenuItem,
+    MenuList,
+    Popover,
+    PopoverArrow,
+    PopoverBody,
+    PopoverCloseButton,
+    PopoverContent,
+    PopoverFooter,
+    PopoverHeader,
+    PopoverTrigger,
+    Portal,
+    Wrap,
+    WrapItem,
+} from "@chakra-ui/react";
+import { toast } from "react-toastify";
+import useInterval from "../../hooks/useInterval";
+import { AuthContext } from "../../contexts/AuthContext";
+import api from "../../services/api";
 
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { MdOutlineExitToApp } from "react-icons/md";
+import { FaCheck } from "react-icons/fa";
 
-import { AuthContext } from "../../contexts/AuthContext";
+import { NotificationType } from "../../pages/Notifications";
 
-import { Container } from "./styles";
+import { Container, Notification, NotificationItem } from "./styles";
 
 const Header = () => {
-    const { user, signOut } = useContext(AuthContext);
     const navigate = useNavigate();
+    const { user, signOut } = useContext(AuthContext);
+    const [newNotifications, setNewNotifications] = useState<NotificationType[]>([]);
 
     const logOut = useCallback(async () => {
         await signOut();
         navigate("/");
     }, [navigate, signOut]);
 
+    const getNotifications = useCallback(async () => {
+        await api
+            .get(`/notificacao/`)
+            .then((response) => {
+                const userNotifications = response.data.filter((notification: NotificationType) => {
+                    return notification.funcao_notificacao === user.funcao_usuario;
+                });
+
+                const newNotifications = userNotifications.filter(
+                    (notification: NotificationType) => {
+                        return !notification.notificado;
+                    }
+                );
+
+                setNewNotifications(newNotifications);
+            })
+            .catch((error) => {
+                toast.error("Erro na notificação");
+            });
+    }, []);
+
+    const notificated = useCallback(
+        async (nt: NotificationType) => {
+            const dataNotification = {
+                title: nt.title,
+                mensagem: nt.mensagem,
+                notificado: true,
+            };
+
+            await api
+                .put(`/notificacao/${nt.id}/`, dataNotification)
+                .then((response) => {
+                    const restNotifications = newNotifications.filter((notification) => {
+                        return notification.id !== nt.id;
+                    });
+
+                    setNewNotifications(restNotifications);
+                })
+                .catch((error) => {
+                    toast.error("Erro na notificação");
+                });
+        },
+        [newNotifications]
+    );
+
+    const handleLink = () => {
+        navigate("/notificacoes");
+    };
+
+    useInterval(() => {
+        getNotifications();
+    }, 15000);
+
+    useEffect(() => {
+        getNotifications();
+    }, []);
+
     return (
         <Container>
-            <button className="notification_icon">
-                <IoMdNotificationsOutline />
-            </button>
+            <Popover placement="bottom-end">
+                <PopoverTrigger>
+                    <Notification>
+                        <IoMdNotificationsOutline />
+                        {newNotifications.length > 0 ? (
+                            <span className="badge">{newNotifications.length}</span>
+                        ) : (
+                            <div />
+                        )}
+                    </Notification>
+                </PopoverTrigger>
+                <Portal>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverHeader fontWeight="semibold">
+                            <h2>Notificações</h2>
+                        </PopoverHeader>
+                        <PopoverCloseButton />
+                        <PopoverBody>
+                            {newNotifications.map((newNotification, index) => {
+                                return (
+                                    <NotificationItem key={index}>
+                                        <div>
+                                            <div className="title">{newNotification.title}</div>
+                                            <div className="description">
+                                                {newNotification.mensagem.length >= 22
+                                                    ? `${newNotification.mensagem.substring(
+                                                          0,
+                                                          22
+                                                      )}...`
+                                                    : newNotification.mensagem}
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <IconButton
+                                                variant="outline"
+                                                colorScheme="teal"
+                                                aria-label="Check"
+                                                size="xs"
+                                                icon={<FaCheck />}
+                                                onClick={() => notificated(newNotification)}
+                                            />
+                                        </div>
+                                    </NotificationItem>
+                                );
+                            })}
+                        </PopoverBody>
+                        <PopoverFooter display="flex" justifyContent="center">
+                            <Link color="#fab039" fontSize="15px" onClick={handleLink}>
+                                Histórico de Notificações
+                            </Link>
+                        </PopoverFooter>
+                    </PopoverContent>
+                </Portal>
+            </Popover>
             <Menu>
                 <MenuButton>
                     <Wrap>
@@ -32,9 +167,11 @@ const Header = () => {
                     </Wrap>
                 </MenuButton>
                 <MenuList>
-                    <MenuItem className="button_logout" onClick={logOut}>
-                        <MdOutlineExitToApp /> <span>Sair</span>
-                    </MenuItem>
+                    <MenuGroup title={user.username} marginBottom="15px">
+                        <MenuItem className="button_logout" onClick={logOut}>
+                            <MdOutlineExitToApp /> <span>Sair</span>
+                        </MenuItem>
+                    </MenuGroup>
                 </MenuList>
             </Menu>
         </Container>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -10,14 +10,43 @@ export const Container = styled.div`
     padding-right: 50px;
     border-bottom: solid 1px #c7c7c7;
 
-    .notification_icon {
-        font-size: 25px;
-        margin-right: 20px;
-    }
-
     .button_logout {
         > span {
             margin-left: 10px;
         }
+    }
+`;
+
+export const Notification = styled.button`
+    font-size: 25px;
+    margin-right: 20px;
+
+    .badge {
+        position: absolute;
+        top: 12px;
+        background-color: rgba(212, 19, 13, 1);
+        color: #fff;
+        border-radius: 3px;
+        padding: 1px 3px;
+        font-size: 8px;
+    }
+`;
+
+export const NotificationItem = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin: 10px 0 20px 0;
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 1px 1px 5px 0.5px rgba(0, 0, 0, 0.1);
+
+    .title {
+        font-weight: 500;
+    }
+
+    .description {
+        color: #b3b3b3;
     }
 `;
